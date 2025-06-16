96b20b1e17e7f84664afa07562bd3dbf589ee516
anish <anishchandragiri@gmail.com>
2025-06-17 02:41:34 +0530

    kj

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Required for Capacitor static export
+  // output: 'export', // Required for Capacitor static export
   typescript: {
     ignoreBuildErrors: true,
   },
